3c61871868b8457485c5a5ffc49473323d6a848a
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2025-10-22 19:48:35 +0700

    feat: implement database from pdm

---
+++ app/Models/Pin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Pin extends Model
+{
+    protected $fillable = [
+        'image_url',
+        'title',
+        'description'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function board(): BelongsToMany
+    {
+        return $this->belongsToMany(Board::class, 'board_pin')->withTimestamps();
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likedBy(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'likes');
+    }
+}
