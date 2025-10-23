abe3663794ab577c0a9b32324a9454397192e3b6
erlanggardr <erlanggarizqidwi@gmail.com>
2025-10-24 01:04:20 +0700

    Fix: Force HTTPS in production and trust Railway proxies for HTTPS detection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        // Trust Railway proxies for HTTPS detection
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
